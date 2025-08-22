38dd07d7bf2cd04e9b39bc81f79d72bd8aa7140e
Jackson Wilson <jacksonwilson99@gmail.com>
2025-08-21 22:37:57 -0500

    init push

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Home from './components/Home';
+import About from './components/About';
+import Blog from './components/Blog';
+import './App.css';
+
+const App: React.FC = () => {
+  return (
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/blog" element={<Blog />} />
+      </Routes>
+    </Router>
+  );
+};
+
+export default App;
